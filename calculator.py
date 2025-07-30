fc2b760d20e9dd009f7e77891a7d30a399e4dbb6
Conroy Makhubu <cmakhubu@gmail.com>
2025-07-30 15:37:14 +0200

    Update calculator.py
    testing operation

--- calculator.py
+++ calculator.py
@@ -4,3 +4,13 @@ first_number = float(input("Enter first number: "))
 second_number = float(input("Enter second number: "))
 #ask for a choice of operation
 operation = input("Enter operation (+, -, *, /): ")
+
+#testing the operation
+if operation == '+':
+    result = f"{first_number} + {second_number} = {first_number + second_number}"
+elif operation == '-':
+    result = f"{first_number} - {second_number} = {first_number - second_number}"
+elif (operation == '*'):
+    result = f"{first_number} * {second_number} = {first_number * second_number}"
+elif (operation == '/'):
+  # comment:     
