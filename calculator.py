1f0433a24b659be98faad3d0718afe132394f5ee
Conroy Makhubu <cmakhubu@gmail.com>
2025-07-30 15:32:39 +0200

    Update calculator.py
    add third input

--- calculator.py
+++ calculator.py
@@ -2,3 +2,5 @@
 first_number = float(input("Enter first number: "))
 #ask input number from user
 second_number = float(input("Enter second number: "))
+#ask for a choice of operation
+operation = input("Enter operation (+, -, *, /): ")
